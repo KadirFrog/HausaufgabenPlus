62dcbe4094601b584ad98d51345f924c60ae776f
KadirFrog <yanarkadiraras@gmail.com>
2023-08-09 00:19:54 +0300

    RELEASE V1.0

--- haplus/models.py
+++ haplus/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     account_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    school_id = models.CharField(max_length=10, blank=True, default=0)
+    school_id = models.CharField(max_length=10, blank=True, default='0')  # Default value as a string
     school_class = models.CharField(max_length=10, blank=True)
     telephone_number = models.CharField(max_length=15, blank=True)
+    can_post = models.DecimalField(max_digits=1, decimal_places=0, default=1)
     
     # Add related_name attributes to avoid clashes
     groups = models.ManyToManyField(Group, related_name='users')
